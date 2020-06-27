b42d16b782e12e7c387e7d821ec4a937bb29ed5c
Daniel Curtis <daniel17curtis@gmail.com>
2020-06-27 10:54:43 -0400

    Added minesweeper route

--- src/components/IndexRoutes.js
+++ src/components/IndexRoutes.js
@@ -1,64 +1,59 @@
 import React, { useState } from 'react';
-import { Link, graphql } from 'gatsby';
+import Draggable from 'react-draggable';
 import '98.css';
 import './index.css';
-
-import Layout from './Layout';
-import SEO from './seo';
-
-import Icons from './Icons';
 import About from './About';
 import Articles from './Articles';
 import Projects from './Projects';
+import Minesweeper from './Minesweeper';
+import Icons from './children/Icons';
 
 function IndexRoutes() {
-  const [active, setActive] = useState('About Me');
-
+  const [active, setActive] = useState('Articles');
   let component;
+
   if (active === 'About Me') component = <About />;
   else if (active === 'Articles') component = <Articles />;
   else if (active === 'Projects') component = <Projects />;
+  else if (active === 'Minesweeper') component = <Minesweeper />;
 
   return (
     <div>
       <Icons setActive={setActive} />
       {active && (
-        <div
-          className="window"
-          style={{
-            maxWidth: '960px',
-            zIndex: 1,
-            left: '0',
-            right: '0',
-            top: '5vh',
-            position: 'absolute',
-            margin: 'auto',
-          }}
-        >
-          <div className="title-bar">
-            <div className="title-bar-text">{active}</div>
-            <div className="title-bar-controls">
-              <button
-                aria-label="Close"
-                onClick={() => setActive(null)}
-              ></button>
+        <Draggable handle="strong">
+          <div
+            className={`window ${
+              active === 'Minesweeper' ? 'mine-win' : 'main-win'
+            }`}
+          >
+            <div className="title-bar">
+              <strong>
+                <div className="title-bar-text">{active}</div>
+              </strong>
+              <div className="title-bar-controls">
+                <button
+                  aria-label="Close"
+                  onClick={() => setActive(null)}
+                ></button>
+              </div>
+            </div>
+            <div
+              className="window-body"
+              style={{
+                fontFamily: 'Arial',
+                fontSize: '16px',
+                WebkitFontSmoothing: 'auto',
+                maxHeight: '80vh',
+                overflowY: 'scroll',
+                margin: '0 0 0 10px',
+                paddingBottom: '15px',
+              }}
+            >
+              {component}
             </div>
           </div>
-          <div
-            className="window-body"
-            style={{
-              fontFamily: 'Arial',
-              fontSize: '14px',
-              webkitFontSmoothing: 'auto',
-              maxHeight: '80vh',
-              overflowY: 'scroll',
-              margin: '0 0 0 10px',
-              paddingBottom: '15px',
-            }}
-          >
-            {component}
-          </div>
-        </div>
+        </Draggable>
       )}
     </div>
   );
