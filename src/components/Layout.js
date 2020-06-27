3bb4d3a9c77649f089b6ae0679c9d274eda2992d
Daniel Curtis <daniel17curtis@gmail.com>
2020-06-27 10:54:58 -0400

    Moved logic to index routes

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,38 +1,10 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { useStaticQuery, graphql } from 'gatsby';
-import Footer from './Footer';
 import '98.css';
 import './index.css';
 
 function Layout({ children }) {
-  const [colorMode, setColorMode] = useState('Sunshine');
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `);
-
-  const updateColorMode = () => {
-    if (colorMode === 'Sunshine') setColorMode('Moonshine');
-    else if (colorMode === 'Moonshine') setColorMode('Retrotime');
-    else if (colorMode === 'Retrotime') setColorMode('Sunshine');
-  };
-
-  //       <Header siteTitle={data.site.siteMetadata.title} colorMode={colorMode} />
-
-  return (
-    <div className={colorMode}>
-      <div>
-        <main>{children}</main>
-      </div>
-      <Footer />
-    </div>
-  );
+  return <main>{children}</main>;
 }
 
 Layout.propTypes = { children: PropTypes.node.isRequired };
