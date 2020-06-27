4b18bb567c18b29c6dee547d1b17535ba098bb2f
Daniel Curtis <daniel17curtis@gmail.com>
2020-06-27 10:55:29 -0400

    Added JSON data for projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,25 +1,60 @@
 import React from 'react';
 
-function Projects() {
+function Project({ name, desc }) {
   return (
-    <div>
-      <h1>Projects</h1>
+    <div className="Project">
+      <h2 className="Project-h2">{name}</h2>
+      <ul>
+        {desc.map((el) => (
+          <li key={el}>{el}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Projects() {
+  const data = [
+    {
+      name: 'Taskaholic - Personal project and time management',
+      desc: [
+        'Built using React, NodeJS, and MongoDB',
+        'Hosted with Docker and Kubernetes in AWS',
+      ],
+    },
+    {
+      name: "CS Dragon - Learn CS topics through Drag 'n Drop",
+      desc: [
+        'Teaches popular data structures and algorithms',
+        "Built using React and Atlassian's react-beautiful-dnd",
+      ],
+    },
+    {
+      name:
+        'Another C Library - A C library for building complex, scalable applications',
+      desc: [
+        'Co-creator of the Open Source library',
+        'Built and created the documentation website and tutorial videos',
+      ],
+    },
+    {
+      name: 'Bullish Dev - Machine Learning stock predictor',
+      desc: [
+        'Stock predictor soley based on deep learning models',
+        'Built using NumPy and TensorFlow',
+      ],
+    },
+  ];
+
+  return (
+    <div style={{ padding: '3vh 7vw' }}>
+      <h1 style={{ marginBottom: '0' }}>Projects</h1>
       <p>
-        I'm a project-based learner. I'm always hacking away on some side
-        project, here's a few recent ones:
+        I'm always hacking away on side projects. Here's a few of my favorites:
       </p>
-      <table style={{ textAlign: 'left' }}>
-        <thead>
-          <th>Project</th>
-          <th style={{ borderSpacing: '20px' }}>Description</th>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Another C Library</td>
-            <td>Open source library that makes C accessible</td>
-          </tr>
-        </tbody>
-      </table>
+      {data.map((el, i) => (
+        <Project key={i} name={el.name} desc={el.desc} />
+      ))}
     </div>
   );
 }
