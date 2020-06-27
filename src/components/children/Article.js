15bbf831f1bc5370c5aeef16a6671e7533a43e3b
Daniel Curtis <daniel17curtis@gmail.com>
2020-06-27 10:55:54 -0400

    Removed duplicate articles

--- src/components/children/Article.js
+++ src/components/children/Article.js
@@ -15,18 +15,6 @@ function Article({ data }) {
       </div>
       <p>{data.description}</p>
       <hr />
-
-      {/* delete below for prod */}
-      <a href={data.url} target="_blank" rel="noreferrer">
-        <h2 className="Article-h2">{data.title}</h2>
-      </a>
-      <div className="Article-title">
-        <FcLike size="1.2em" style={{ margin: '0 1px 3px 0' }} />
-        <code>{data.public_reactions_count}</code>
-        <code style={{ marginLeft: '12px' }}>{data.readable_publish_date}</code>
-      </div>
-      <p>{data.description}</p>
-      <hr />
     </div>
   );
 }
