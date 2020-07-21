a151e44df684fec14d137bf33324c1f75e2296c8
Daniel Curtis <daniel17curtis@gmail.com>
2020-07-21 18:16:01 -0400

    Updated for a11y

--- src/minesweeper/components/Smiley.js
+++ src/minesweeper/components/Smiley.js
@@ -5,79 +5,76 @@ import { GameContext } from './GameContext';
 import { genMinefield } from '../utils';
 
 function Smiley() {
-  const images = useStaticQuery(graphql`
-    query {
-      smile: file(relativePath: { eq: "smile.png" }) {
-        childImageSharp {
-          fluid(maxWidth: 35, maxHeight: 35) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-      sunglasses: file(relativePath: { eq: "sunglasses.png" }) {
-        childImageSharp {
-          fluid(maxWidth: 35, maxHeight: 35) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-      dead: file(relativePath: { eq: "dead.png" }) {
-        childImageSharp {
-          fluid(maxWidth: 35, maxHeight: 35) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-    }
-  `);
-  const { state, dispatch } = useContext(GameContext);
-  const { win, gameover } = state;
+	const images = useStaticQuery(graphql`
+		query {
+			smile: file(relativePath: { eq: "smile.png" }) {
+				childImageSharp {
+					fluid(maxWidth: 35, maxHeight: 35) {
+						...GatsbyImageSharpFluid
+					}
+				}
+			}
+			sunglasses: file(relativePath: { eq: "sunglasses.png" }) {
+				childImageSharp {
+					fluid(maxWidth: 35, maxHeight: 35) {
+						...GatsbyImageSharpFluid
+					}
+				}
+			}
+			dead: file(relativePath: { eq: "dead.png" }) {
+				childImageSharp {
+					fluid(maxWidth: 35, maxHeight: 35) {
+						...GatsbyImageSharpFluid
+					}
+				}
+			}
+		}
+	`);
+	const { state, dispatch } = useContext(GameContext);
+	const { win, gameover } = state;
 
-  const resetGame = () => {
-    const minefield = genMinefield();
-    const mines = minefield.filter((mine) => mine.bomb === true).length;
+	const resetGame = () => {
+		const minefield = genMinefield();
+		const mines = minefield.filter((mine) => mine.bomb === true).length;
 
-    dispatch({ type: 'setMinefield', payload: minefield });
-    dispatch({ type: 'setMines', payload: mines });
-    dispatch({ type: 'setFlags', payload: 0 });
-    dispatch({ type: 'setTime', payload: 0 });
+		dispatch({ type: 'setMinefield', payload: minefield });
+		dispatch({ type: 'setMines', payload: mines });
+		dispatch({ type: 'setFlags', payload: 0 });
+		dispatch({ type: 'setTime', payload: 0 });
 
-    gameover
-      ? dispatch({ type: 'setGameover', payload: false })
-      : dispatch({ type: 'setWin', payload: false });
-  };
+		gameover
+			? dispatch({ type: 'setGameover', payload: false })
+			: dispatch({ type: 'setWin', payload: false });
+	};
 
-  if (gameover) {
-    return (
-      <div onClick={resetGame}>
-        <Img
-          className="Smiley-img"
-          fluid={images['dead'].childImageSharp.fluid}
-          alt={'dead face'}
-        />
-      </div>
-    );
-  } else if (win) {
-    return (
-      <div onClick={resetGame}>
-        <Img
-          className="Smiley-img"
-          fluid={images['sunglasses'].childImageSharp.fluid}
-          alt={'sunglasses face'}
-        />
-      </div>
-    );
-  } else {
-    return (
-      <div onClick={resetGame}>
-        <Img
-          className="Smiley-img"
-          fluid={images['smile'].childImageSharp.fluid}
-          alt={'smiley face'}
-        />
-      </div>
-    );
-  }
+	if (gameover) {
+		return (
+			<Img
+				className="Smiley-img"
+				fluid={images['dead'].childImageSharp.fluid}
+				alt={'dead face'}
+				onClick={resetGame}
+			/>
+		);
+	} else if (win) {
+		return (
+			<Img
+				className="Smiley-img"
+				fluid={images['sunglasses'].childImageSharp.fluid}
+				alt={'sunglasses face'}
+				onClick={resetGame}
+			/>
+		);
+	} else {
+		return (
+			<Img
+				className="Smiley-img"
+				fluid={images['smile'].childImageSharp.fluid}
+				alt={'smiley face'}
+				onClick={resetGame}
+			/>
+		);
+	}
 }
 
 export default Smiley;
